bd0ed97b5964cc0d3963f157e619856baa804e0f
Eduardo Antonio <126982066+eduhantonio@users.noreply.github.com>
2023-11-23 10:31:35 -0300

    Add files via upload

---
+++ Randomicos/Randomicos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numAleatorio = new Random();
+
+            int numero = numAleatorio.Next(1, 10);
+            Console.WriteLine(numero);
+            Console.ReadKey();
+        }
+    }
+}
